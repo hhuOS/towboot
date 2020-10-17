89b16f53a32f0009599a99ac97587d5fdac044ac
Niklas Sombert <niklas.sombert@uni-duesseldorf.de>
2020-10-17 20:36:01 +0200

    build.sh: Comment out the x86_64-related stuff

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -eu
 cargo build
-cargo build --target x86_64-unknown-uefi
+# cargo build --target x86_64-unknown-uefi
 
 mkdir -p image
 
@@ -13,9 +13,10 @@ sudo mkfs.vfat ${LOOP}p1
 sudo mount ${LOOP}p1 image/
 sudo mkdir -p image/efi/boot
 sudo cp target/i686-unknown-uefi/debug/bootloader.efi image/efi/boot/bootia32.efi
-sudo cp target/x86_64-unknown-uefi/debug/bootloader.efi image/efi/boot/bootx64.efi
+# sudo cp target/x86_64-unknown-uefi/debug/bootloader.efi image/efi/boot/bootx64.efi
 sudo cp bootloader.toml image/bootloader.toml
 sudo umount image/
 sudo losetup -d $LOOP
 
 qemu-system-i386 -machine pc,accel=kvm,kernel-irqchip=off -bios ~/bin/OVMF.bin -hda image.img
+# qemu-system-x86_64 -machine pc,accel=kvm,kernel-irqchip=off -bios /usr/share/ovmf/OVMF.fd -hda image.img
