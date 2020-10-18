21d92c91cabf011f8a6d401d615ee7f8962d7d7b
Niklas Sombert <niklas.sombert@uni-duesseldorf.de>
2020-10-18 19:43:20 +0200

    Use mkgpt for building the image
    Now, we don't need root access anymore.

--- build.sh
+++ build.sh
@@ -3,23 +3,37 @@ set -eu
 cargo build
 # cargo build --target x86_64-unknown-uefi
 
-mkdir -p image
+echo "checking whether mkgpt exists and building it if not…"
+if [ ! -d mkgpt ]
+then
+    git clone https://github.com/jncronin/mkgpt.git
+fi
+if [ ! -f mkgpt/mkgpt ]
+then
+    cd mkgpt
+    aclocal
+    automake --add-missing
+    ./configure
+    make
+    cd ..
+fi
 
-dd if=/dev/zero of=image.img count=50 bs=1M
-parted image.img mktable gpt
-echo ignore | parted image.img mkpart fat32 1 100%
-LOOP=$(sudo losetup --partscan --find image.img --show)
-sudo mkfs.vfat ${LOOP}p1
-sudo mount ${LOOP}p1 image/
-sudo mkdir -p image/efi/boot
-sudo cp target/i686-unknown-uefi/debug/bootloader.efi image/efi/boot/bootia32.efi
-# sudo cp target/x86_64-unknown-uefi/debug/bootloader.efi image/efi/boot/bootx64.efi
-sudo cp bootloader.toml image/bootloader.toml
-sudo cp ../../kernels/multiboot1.elf image/
-sudo cp ~/dev/hhuOS/loader/boot/hhuOS.bin image/
-sudo cp ~/dev/hhuOS/loader/boot/hhuOS.initrd image/
-sudo umount image/
-sudo losetup -d $LOOP
+echo "building image…"
+dd if=/dev/zero of=part.img count=50 bs=1M
+mkfs.vfat part.img
 
+mmd -i part.img efi
+mmd -i part.img efi/boot
+mcopy -i part.img target/i686-unknown-uefi/debug/bootloader.efi ::efi/boot/bootia32.efi
+mcopy -i part.img target/x86_64-unknown-uefi/debug/bootloader.efi ::efi/boot/bootx64.efi
+mcopy -i part.img bootloader.toml ::
+mcopy -i part.img ../../kernels/multiboot1.elf ::
+mcopy -i part.img ~/dev/hhuOS/loader/boot/hhuOS.bin ::
+mcopy -i part.img ~/dev/hhuOS/loader/boot/hhuOS.initrd ::
+
+mkgpt/mkgpt -o image.img --part part.img --type system
+rm part.img
+
+echo "launching qemu…"
 qemu-system-i386 -machine pc,accel=kvm,kernel-irqchip=off -bios ~/bin/OVMF.bin -hda image.img
 # qemu-system-x86_64 -machine pc,accel=kvm,kernel-irqchip=off -bios /usr/share/ovmf/OVMF.fd -hda image.img
