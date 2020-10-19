5c9e1bb23d50d1287b13f9031faa396f3fde2ef5
Niklas Sombert <niklas.sombert@uni-duesseldorf.de>
2020-10-19 21:27:53 +0200

    Update build script to download OVMF if it's missing

--- build.sh
+++ build.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 set -eu
 
+OVMF_i686_RPM_FILE_NAME="edk2-ovmf-ia32-20190501stable-4.fc31.noarch.rpm"
+OVMF_i686_RPM_URL="https://download-ib01.fedoraproject.org/pub/fedora/linux/releases/31/Everything/x86_64/os/Packages/e/$OVMF_i686_RPM_FILE_NAME"
+
 BUILD=${BUILD:-debug}
 if [ $BUILD = "release" ]
 then
@@ -14,7 +17,7 @@ if [ $ARCH = "i686" ]
 then
     EFIARCH="ia32"
     QEMUARCH="i386"
-    OVMF_PATH=~/bin/OVMF.bin
+    OVMF_PATH="OVMF.fd"
 elif [ $ARCH = "x86_64" ]
 then
     EFIARCH="x64"
@@ -54,6 +57,34 @@ mcopy -i part.img ~/dev/hhuOS/loader/boot/hhuOS.initrd ::
 
 mkgpt/mkgpt -o image.img --part part.img --type system
 rm part.img
+
+echo "checking whether OVMF exists, else trying to download…"
+if [ ! -f $OVMF_PATH ]
+then
+    if [ $ARCH = "i686" ]
+    then
+        wget $OVMF_i686_RPM_URL
+        if [ -n $(which rpm2cpio) ]
+        then
+            rpm2cpio $OVMF_i686_RPM_FILE_NAME > $OVMF_i686_RPM_FILE_NAME.cpio
+        elif [ -n $(which unzstd) ]
+        then
+            7z x $OVMF_i686_RPM_FILE_NAME
+            unzstd *.zstd -o $OVMF_i686_RPM_FILE_NAME.cpio # TODO
+        else
+            echo "You'll need either rpm2cpio or 7z and unzstd (or just drop a file to $OVMF_PATH)."
+            return 1
+        fi
+        rm $OVMF_i686_RPM_FILE_NAME
+        cpio --extract --file $OVMF_i686_RPM_FILE_NAME.cpio -d
+        rm $OVMF_i686_RPM_FILE_NAME.cpio
+        mv usr/share/edk2/ovmf-ia32/OVMF_CODE.fd OVMF.fd
+        rm -r usr
+    else
+        echo "Don't know where to download OVMF for $ARCH to $OVMF_PATH."
+        return 1
+    fi
+fi
 
 echo "launching qemu…"
 qemu-system-$QEMUARCH -machine pc,accel=kvm,kernel-irqchip=off -bios $OVMF_PATH -hda image.img
