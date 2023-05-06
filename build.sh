b24335ac8117fbd8168375e4cf74831cfc08c2e9
Niklas Sombert <niklas.sombert@uni-duesseldorf.de>
2023-05-06 15:51:58 +0200

    Copy OVMF download from hhuOS

--- build.sh
+++ build.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 set -eu
 
-OVMF_i686_RPM_FILE_NAME="edk2-ovmf-ia32-20200801stable-1.fc33.noarch.rpm"
-OVMF_i686_RPM_URL="https://download-ib01.fedoraproject.org/pub/fedora/linux/releases/33/Everything/x86_64/os/Packages/e/$OVMF_i686_RPM_FILE_NAME"
-
 BUILD=${BUILD:-debug}
 if [ $BUILD = "release" ]
 then
@@ -17,12 +14,10 @@ if [ $ARCH = "i686" ]
 then
     EFIARCH="ia32"
     QEMUARCH="i386"
-    OVMF_PATH="OVMF.fd"
 elif [ $ARCH = "x86_64" ]
 then
     EFIARCH="x64"
     QEMUARCH=$ARCH
-    OVMF_PATH="/usr/share/ovmf/OVMF.fd"
 else
     echo "unknown arch $ARCH"
     return 1
@@ -80,39 +75,17 @@ mcopy -i part.img "$KERNEL" ::multiboot.elf
 mkgpt/mkgpt -o image.img --part part.img --type system
 rm part.img
 
-echo "checking whether OVMF exists, else trying to download…"
-if [ ! -f $OVMF_PATH ]
-then
-    if [ $ARCH = "i686" ]
-    then
-        wget $OVMF_i686_RPM_URL
-        if [ -n $(which rpm2cpio) ]
-        then
-            rpm2cpio $OVMF_i686_RPM_FILE_NAME > $OVMF_i686_RPM_FILE_NAME.cpio
-        elif [ -n $(which unzstd) ]
-        then
-            7z x $OVMF_i686_RPM_FILE_NAME
-            unzstd *.zstd -o $OVMF_i686_RPM_FILE_NAME.cpio # TODO
-        else
-            echo "You'll need either rpm2cpio or 7z and unzstd (or just drop a file to $OVMF_PATH)."
-            return 1
-        fi
-        rm $OVMF_i686_RPM_FILE_NAME
-        cpio --extract --file $OVMF_i686_RPM_FILE_NAME.cpio -d
-        rm $OVMF_i686_RPM_FILE_NAME.cpio
-        mv usr/share/edk2/ovmf-ia32/OVMF_CODE.fd OVMF.fd
-        rm -r usr
-    else
-        echo "Don't know where to download OVMF for $ARCH to $OVMF_PATH."
-        return 1
-    fi
-fi
+echo "downloading OVMF if needed"
+cd ovmf/ && bash download.sh; cd ..
+
+FIRMWARE=${FIRMWARE:-ovmf/$EFIARCH/OVMF.fd}
+echo "Using $FIRMWARE as the firmware, set FIRMWARE to override."
 
 echo "launching qemu with KVM=$KVM…"
 if [ $GDB = "yes" ]
 then
     echo "The machine starts paused, waiting for GDB to attach to localhost:1234."
 fi
-qemu-system-$QEMUARCH $QEMUMACHINE $QEMUDEBUG -bios $OVMF_PATH \
+qemu-system-$QEMUARCH $QEMUMACHINE $QEMUDEBUG -bios "$FIRMWARE" \
 -serial stdio \
 -drive driver=raw,node-name=disk,file.driver=file,file.filename=image.img -m 256
