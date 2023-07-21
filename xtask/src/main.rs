3c563cd894e25fc48eed89bbece062c7f52c52b4
Niklas Sombert <niklas.sombert@uni-duesseldorf.de>
2023-07-21 16:22:57 +0200

    Basic subcommand setup

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -1,3 +1,35 @@
-fn main() {
-    println!("Hello, world!");
+use cli_xtask::clap;
+use cli_xtask::config::Config;
+use cli_xtask::{Result, Run, Xtask};
+
+fn main() -> Result<()> {
+    Xtask::<Command>::main()
+}
+
+#[derive(Debug, clap::Subcommand)]
+enum Command {
+    Build,
+    Run,
+}
+
+impl Run for Command {
+    fn run(&self, _config: &Config) -> Result<()> {
+        match self {
+            Self::Build => println!("build!"),
+            Self::Run => println!("run!"),
+        }
+        Ok(())
+    }
+
+    fn into_any(self: Box<Self>) -> Box<dyn std::any::Any> {
+        self
+    }
+
+    fn as_any(&self) -> &dyn std::any::Any {
+        self
+    }
+
+    fn as_any_mut(&mut self) -> &mut dyn std::any::Any {
+        self
+    }
 }
