4ce3714bb8cacdb81ec004410c7a8ce34ab6e6f3
Niklas Sombert <niklas.sombert@uni-duesseldorf.de>
2023-10-24 21:00:21 +0200

    xtask: Use argh

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -6,7 +6,7 @@ use std::path::PathBuf;
 use std::process;
 
 use anyhow::{Result, anyhow};
-use clap::{Subcommand, Parser, command, ValueEnum};
+use argh::{FromArgs, from_env};
 use log::info;
 use tempfile::NamedTempFile;
 
@@ -17,86 +17,92 @@ use image::Image;
 
 const DEFAULT_IMAGE_SIZE: u64 = 50*1024*1024;
 
-#[derive(Debug, Parser)]
-#[command(name = "cargo xtask")]
+#[derive(Debug, FromArgs)]
+/// Top-level command.
 struct Cli {
-    #[command(subcommand)]
+    #[argh(subcommand)]
     command: Command,
 }
 
-#[derive(Debug, Clone, Copy, ValueEnum)]
+#[derive(Debug, Clone, Copy, strum::EnumString, strum::Display)]
 enum Arch {
     I686, X86_64,
 }
 
-#[derive(Debug, Subcommand)]
+#[derive(Debug, FromArgs)]
+#[argh(subcommand)]
 enum Command {
-    /// Build a bootable image, containing towboot, a kernel and modules
-    Build {
-        #[arg( long )]
-        release: bool,
-        #[arg( long )]
-        no_i686: bool,
-        #[arg( long )]
-        no_x86_64: bool,
-        #[arg( long, default_value = "image.img" )]
-        target: PathBuf,
-        #[arg( last = true, default_value = "-config towboot.toml" )]
-        runtime_args: Vec<String>,
-    },
-    Run {
-        #[arg( long, default_value = "image.img" )]
-        image: PathBuf,
-        #[arg( long, default_value = "i686" )]
-        arch: Arch,
-        #[arg( long )]
-        kvm: bool,
-        #[arg( long )]
-        gdb: bool,
-        #[arg( long )]
-        /// use the specified firmware instead of OVMF
-        firmware: Option<PathBuf>,
-    },
+    Build(Build),
+    Run(Run),
 }
-impl Command {
-    fn do_build(release: bool, no_i686: bool, no_x86_64: bool, target: PathBuf, runtime_args: Vec<String>) -> Result<()> {
+
+#[derive(Debug, FromArgs)]
+#[argh(subcommand, name = "build")]
+/// Build a bootable image containing, towboot, kernels and their modules.
+struct Build {
+    /// do release builds
+    #[argh(switch)]
+    release: bool,
+
+    /// do not include i686 build
+    #[argh(switch)]
+    no_i686: bool,
+
+    /// do not include x86_64 build
+    #[argh(switch)]
+    no_x86_64: bool,
+
+    /// where to place the image
+    #[argh(option, default = "PathBuf::from(\"image.img\")")]
+    target: PathBuf,
+
+    /// runtime options to pass to towboot
+    #[argh(positional, greedy)]
+    runtime_args: Vec<String>,
+}
+
+impl Build {
+    fn r#do(self) -> Result<()> {
         let mut cargo_command = process::Command::new("cargo");
         let mut build_command = cargo_command.arg("build");
-        if release {
+        if self.release {
             build_command = cargo_command.arg("--release");
         }
-        if !no_i686 {
+        if !self.no_i686 {
             info!("building for i686, pass --no-i686 to skip this");
             build_command
                 .arg("--target")
                 .arg("i686-unknown-uefi")
                 .spawn()?.wait()?;
         }
-        if !no_x86_64 {
+        if !self.no_x86_64 {
             info!("building for x86_64, pass --no-x86-64 to skip this");
             build_command
                 .arg("--target")
                 .arg("x86_64-unknown-uefi")
                 .spawn()?.wait()?;
         }
-        info!("creating image at {}", target.display());
-        let mut image = Image::new(&target, DEFAULT_IMAGE_SIZE)?;
-        let build = match release {
+        info!("creating image at {}", self.target.display());
+        let mut image = Image::new(&self.target, DEFAULT_IMAGE_SIZE)?;
+        let build = match self.release {
             true => "release",
             false => "debug",
         };
-        if !no_i686 {
+        if !self.no_i686 {
             let source: PathBuf = ["target", "i686-unknown-uefi", build, "towboot.efi"].into_iter().collect();
             image.add_file(&source, &PathBuf::from("EFI/Boot/bootia32.efi"))?;
         }
-        if !no_x86_64 {
+        if !self.no_x86_64 {
             let source: PathBuf = ["target", "x86_64-unknown-uefi", build, "towboot.efi"].into_iter().collect();
             image.add_file(&source, &PathBuf::from("EFI/Boot/bootx64.efi"))?;
         }
 
         // generate a configuration file from the load options
         let mut load_options = "towboot.efi".to_owned();
-        for string in runtime_args.iter() {
+        if self.runtime_args.is_empty() {
+            load_options.push_str(" -config towboot.toml");
+        }
+        for string in self.runtime_args.iter() {
             load_options.push(' ');
             if string.contains(" ") {
                 load_options.push('"');
@@ -121,34 +127,62 @@ impl Command {
         }
         Ok(())
     }
+}
 
-    fn do_run(image: PathBuf, arch: Arch, kvm: bool, gdb: bool, firmware: Option<PathBuf>) -> Result<()> {
+#[derive(Debug, FromArgs)]
+#[argh(subcommand, name = "run")]
+/// Run an image in a VM
+struct Run {
+    /// what image to boot
+    #[argh(option, default = "PathBuf::from(\"image.img\")")]
+    image: PathBuf,
+
+    /// what architecture to use for the VM
+    #[argh(option, default = "Arch::I686")]
+    arch: Arch,
+
+    /// enable KVM
+    #[argh(switch)]
+    kvm: bool,
+
+    /// wait for GDB to attach
+    #[argh(switch)]
+    gdb: bool,
+
+    /// use the specified firmware instead of OVMF
+    #[argh(option)]
+    firmware: Option<PathBuf>,
+}
+
+
+impl Run {
+    fn r#do(self) -> Result<()> {
         info!("spawning QEMU");
-        let firmware_path: PathBuf = if let Some(path) = firmware {
+        let firmware_path: PathBuf = if let Some(path) = self.firmware {
             assert!(path.exists());
             path.clone()
         } else {
             // TODO: replace this script
             process::Command::new("bash").arg("download.sh")
                 .current_dir("ovmf").spawn()?.wait()?;
-            ["ovmf", match arch {
+            ["ovmf", match self.arch {
                 Arch::I686 => "ia32",
                 Arch::X86_64 => "x64",
             }, "OVMF.fd"].into_iter().collect()
         };
-        let mut qemu_base = process::Command::new(match arch {
+        let mut qemu_base = process::Command::new(match self.arch {
             Arch::I686 => "qemu-system-i386",
             Arch::X86_64 => "qemu-system-x86_64",
         });
         let mut qemu = qemu_base
             .arg("-m").arg("256")
-            .arg("-hda").arg(image)
+            .arg("-hda").arg(self.image)
             .arg("-serial").arg("stdio")
             .arg("-bios").arg(firmware_path);
-        if kvm {
+        if self.kvm {
             qemu = qemu.arg("-machine").arg("pc,accel=kvm,kernel-irqchip=off");
         }
-        if gdb {
+        if self.gdb {
             info!("The machine starts paused, waiting for GDB to attach to localhost:1234.");
             qemu = qemu.arg("-s").arg("-S");
         }
@@ -162,13 +196,9 @@ fn main() -> Result<()> {
         env::set_var("RUST_LOG", "info");
     }
     pretty_env_logger::init();
-    let args = Cli::parse();
+    let args: Cli = from_env();
     match args.command {
-        Command::Build {
-            release, no_i686, no_x86_64, target, runtime_args,
-        } => Command::do_build(release, no_i686, no_x86_64, target, runtime_args),
-        Command::Run {
-            image, arch, kvm, gdb, firmware,
-        } => Command::do_run(image, arch, kvm, gdb, firmware),
+        Command::Build(build) => build.r#do(),
+        Command::Run(run) => run.r#do(),
     }
 }
