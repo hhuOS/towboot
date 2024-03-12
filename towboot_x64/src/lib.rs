9d350c0c34469d68fbe1d4d9b3ce18fc44c6d497
Niklas Sombert <niklas.sombert@uni-duesseldorf.de>
2024-03-12 09:04:24 +0100

    Provide built binaries in crates

---
+++ towboot_x64/src/lib.rs
@@ -0,0 +1,7 @@
+//! This is a dummy crate that contains a towboot binary for x86_64.
+//! 
+//! This is needed because https://github.com/rust-lang/cargo/pull/10061
+//! is not ready yet.
+
+/// The towboot binary for x86_64.
+pub const TOWBOOT: &[u8] = include_bytes!(env!("CARGO_BIN_FILE_TOWBOOT_towboot"));
