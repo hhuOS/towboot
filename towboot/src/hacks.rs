b4d2f7a9b0490bbd408a8ee509a01444dcda4e39
Niklas Sombert <niklas.sombert@uni-duesseldorf.de>
2024-09-03 21:55:26 +0200

    Actually implement the float functions

--- towboot/src/hacks.rs
+++ towboot/src/hacks.rs
@@ -5,19 +5,18 @@
 //! This module contains missing symbols.
 //! 
 //! The fmod and fmodf functions are [currently missing](https://github.com/rust-lang/rust/issues/128533)
-//! on i686-unknown-uefi. In the long run, this should be fixed in
-//! `compiler_builtins`. For now, this monkeypatching seems to be enough.
+//! on i686-unknown-uefi, so let's use the ones of libm.
+//! In the long run, this should be fixed in `compiler_builtins`.
+//! For now, this monkeypatching seems to be enough.
 //!
 //! see https://github.com/rust-lang/compiler-builtins/blob/master/src/math.rs
-//! We could also use libm::fmod{,f} here, but then we'd need __truncdfsf2.
-//! Let's just hope they are never called.
 #[cfg(target_arch = "x86")]
 #[no_mangle]
-pub extern "C" fn fmod(_x: f64, _y: f64) -> f64 {
-    unimplemented!();
+pub extern "C" fn fmod(x: f64, y: f64) -> f64 {
+    libm::fmod(x, y)
 }
 #[cfg(target_arch = "x86")]
 #[no_mangle]
-pub extern "C" fn fmodf(_x: f32, _y: f32) -> f32 {
-    unimplemented!();
+pub extern "C" fn fmodf(x: f32, y: f32) -> f32 {
+    libm::fmodf(x, y)
 }
